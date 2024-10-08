4d507a786f0d843c8a84ebc76a8a101d9eb7cf05
Ferl Jhon D. Javier <80177329+ferlerrorr@users.noreply.github.com>
2024-10-08 18:43:46 +0800

    Update

--- app/Http/Controllers/StoreClientController.php
+++ app/Http/Controllers/StoreClientController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class StoreClientController extends Controller
 {
     public function cardIDSearch($card_id)
     {
-        return response($card_id);
+
+        $response = Hash::make($card_id);
+
+
+        return response($response);
     }
 }
